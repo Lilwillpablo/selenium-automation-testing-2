fc66d251487f14f5b7218e57093b4e02ca35a890
Pavlichek <gaevski321@icloud.com>
2021-03-09 17:13:13 -0500

    setup and teardown

--- page/base_page.py
+++ page/base_page.py
@@ -24,7 +24,7 @@ class BasePage():
         return True
 
     def is_not_element_present(self, how, what,
-                               timeout=4):  # dont forget to commenting up  self.browser.implicitly_wait(timeout) in __init__
+                               timeout=4):
         try:
             WebDriverWait(self.browser, timeout).until(EC.presence_of_element_located((how, what)))
         except TimeoutException:
@@ -33,7 +33,7 @@ class BasePage():
         return False
 
     def is_disappeared(self, how, what,
-                       timeout=4):  # dont forget to commenting up  self.browser.implicitly_wait(timeout) in __init__
+                       timeout=4):
         try:
             WebDriverWait(self.browser, timeout, 1, TimeoutException). \
                 until_not(EC.presence_of_element_located((how, what)))
